50b807b78341c13bb249e55964483373d5075258
yugithub19 <yuneesh.uddeshiya@gmail.com>
2020-10-19 00:52:10 +0530

    Add

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,6 @@
-# -*- encoding: utf-8 -*-
-"""
-License: MIT
-Copyright (c) 2019 - present AppSeed.us
-"""
 
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+admin.site.register(Post)
